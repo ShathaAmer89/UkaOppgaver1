dc03f2be20c2c02553e1d67da1e1f62cedfffd8d
shathaamer <s362064@oslomet.no>
2023-04-04 20:17:04 +0200

    Initial commit

---
+++ src/main/java/com/example/ukaoppgaver1/UkaOppgaver1Application.java
@@ -0,0 +1,13 @@
+package com.example.ukaoppgaver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkaOppgaver1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkaOppgaver1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ukaoppgaver1/UkaOppgaver1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukaoppgaver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkaOppgaver1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
